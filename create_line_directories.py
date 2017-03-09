f422b2b94b7de12a66bf308ecff70f1a6642f2d2
strategy155 <strategy155@gmail.com>
2017-03-09 22:49:32 +0300

    added keras.io nn; not working on big amount of samples; todo fix;

--- create_line_directories.py
+++ create_line_directories.py
@@ -8,29 +8,28 @@ from utils import get_text
 import os
 
 
-def work(rootd,filename):
-    _abs_path = str(os.path.join(rootd, filename))
-    lines = get_text(_abs_path)
-    big_line = ''
-    for line in lines:
-        big_line += line
+def work(root,filename):
+    _abs_path = str(os.path.join(root, filename))
     new_path = os.path.join(os.path.join('D:\\usr\\gwm\\materials\\c_w\\full_strings', _abs_path.split('\\')[-2]),
                             (os.path.splitext(filename)[0] + '_line.pickle'))
-    joblib.dump(big_line, new_path)
+    if not os.path.isfile(new_path):
+        lines = get_text(_abs_path)
+        print(_abs_path)
+        big_line = ''
+        for line in lines:
+            big_line += line
+        joblib.dump(big_line, new_path)
     return None
 
 
 
-def folder(rootd):
-    for filename in rootd[2]:
-        work(rootd[0], filename)
-    return None
 
 
 
 
 
 if __name__ == '__main__':
-    with Pool() as pool:
-        rootd = os.walk('D:\\usr\\gwm\\materials\\c_w\\lib.rus.ec')
-        print(list(pool.map(folder, rootd)))
+    rootd = os.walk('D:\\usr\\gwm\\materials\\c_w\\lib.rus.ec')
+    for root, dirs, filenames in rootd:
+        for filename in filenames:
+            work(root,filename)
--- fit_model.py
+++ fit_model.py
@@ -1,11 +1,15 @@
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import  GaussianNB,MultinomialNB,BernoulliNB
 from sklearn.metrics import classification_report
 import collections
 import random
-
+from keras.models import Sequential
+from keras.layers import Dense,Activation
+import numpy
+import scipy.sparse
 
 CORPUS_PATH = 'D:\\usr\\gwm\\c_w\\lib.rus.ec'
 ALL_CLASSES = ['sf','det','prose','love','adv','child','antique','sci','comp','ref','nonf','religi','humor','home']
@@ -26,16 +30,30 @@ def _choose_filenames(max_number = 1000):
             for i in range(int(9*max_number/10), max_number):
                 X_test_filenames.append(data[key][rnd_list[i]])
                 y_test.append(key)
-    new_cont = list(zip(X_train_filenames,y_train))
-    random.shuffle(new_cont)
-    X_train_filenames[:], y_train[:] = zip(*new_cont)
-    new_cont = list(zip(X_test_filenames,y_test))
-    random.shuffle(new_cont)
-    X_test_filenames[:], y_test[:] = zip(*new_cont)
+    X_train_filenames, y_train = _shuffle_samples(X_train_filenames,y_train)
+    X_test_filenames, y_test = _shuffle_samples(X_test_filenames, y_test)
     prediction_data = collections.namedtuple('Dataset',['X_train','y_train','X_test','y_test'])
     genre_data = prediction_data(X_train=X_train_filenames,y_train=y_train,X_test=X_test_filenames,y_test=y_test)
     return genre_data
 
+
+def norm_names(y):
+    i=0
+    new_dic={}
+    y_new = []
+    for elem in ALL_CLASSES:
+        new_dic[elem] = i
+        i+=1
+    for elem in y:
+        y_new.append(new_dic[elem])
+    return y_new
+
+
+def _shuffle_samples(X,y):
+    meta = list(zip(X,y))
+    random.shuffle(meta)
+    X[:], y[:] = zip(*meta)
+    return X, y
 
 def _gen_docs(filenames):
     c = 0
@@ -46,14 +64,13 @@ def _gen_docs(filenames):
         yield big_line
 
 
-
 def _get_bag_of_word(filenames):
     try:
         tf_idf = joblib.load("tf_idf")
     except FileNotFoundError:
         tf_idf = TfidfVectorizer()
     X = tf_idf.transform(_gen_docs(filenames))
-    return X
+    return X.todense()
 
 
 
@@ -61,12 +78,44 @@ def _get_bag_of_word(filenames):
 
 def main():
     all_data = _choose_filenames()
-    naive_bayes(all_data)
-  #  log_reg(all_data)
+    #  naive_bayes(all_data)
+    # log_reg(all_data)
+    # mlp_clas(all_data)
+    keras(all_data)
+
+
+def keras(all_data):
+    model = Sequential()
+    X_train = _get_bag_of_word(all_data.X_train)
+    X_test = _get_bag_of_word(all_data.X_test)
+    y_train = norm_names(all_data.y_train)
+    y_test = norm_names(all_data.y_test)
+    model.add(Dense(128 ,input_dim=X_train.shape[1],activation='softmax'))
+    model.add(Dense(1))
+    # chunk=numpy.asarray(list(chunks(X_train)))
+    # print(chunk)
+    model.compile(optimizer='sgd',loss='mse')
+    model.fit(X_train, y_train, verbose=2,batch_size=1)
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
+
+
+def chunks(array):
+    for i in range(0,array.shape[1], 1000):
+        try:
+            yield (array[:, i:i+1000].toarray())
+        except IndexError:
+            continue
+
+def mlp_clas(all_data):
+    model = MLPClassifier(verbose=100000, activation='logistic', max_iter=300)
+    model.fit(_get_bag_of_word(all_data.X_train), all_data.y_train)
+    y_pred = model.predict(_get_bag_of_word(all_data.X_test))
+    print(classification_report(all_data.y_test, y_pred))
 
 
 def log_reg(all_data):
-    model = LogisticRegressionCV(verbose=1000, n_jobs=-1, max_iter=10000)
+    model = LogisticRegressionCV(verbose=1000, n_jobs=1, max_iter=10000)
     model.fit(_get_bag_of_word(all_data.X_train), all_data.y_train)
     y_pred = model.predict(_get_bag_of_word(all_data.X_test))
     print(classification_report(all_data.y_test, y_pred))
