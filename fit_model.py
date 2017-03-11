72c22157b59f3ae7dd47907188f7c552ecdf52b4
strategy155 <strategy155@gmail.com>
2017-03-11 18:43:34 +0300

    merge tryhard

--- fit_model.py
+++ fit_model.py
@@ -6,63 +6,47 @@ from sklearn.naive_bayes import  GaussianNB,MultinomialNB,BernoulliNB
 from sklearn.metrics import classification_report
 import collections
 import random
+from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense,Activation
 import numpy
 import scipy.sparse
 
-
 CORPUS_PATH = 'D:\\usr\\gwm\\c_w\\lib.rus.ec'
 ALL_CLASSES = ['sf','det','prose','love','adv','child','antique','sci','comp','ref','nonf','religi','humor','home']
-MAX_NUMBER = 1000
 
-
-def _init_named_tuple():
+def _choose_filenames(max_number = 20):
+    data = joblib.load('new_genre_list')
+    X_train_filenames = []
+    X_test_filenames = []
+    y_train = []
+    y_test = []
+    c = 0
+    for key in data.keys():
+        if len(data[key]) > 1000:
+            rnd_list = random.sample(range(len(data[key])), max_number)
+            for i in range(int(9*max_number/10)):
+                X_train_filenames.append(data[key][rnd_list[i]])
+                y_train.append(key)
+            for i in range(int(9*max_number/10), max_number):
+                X_test_filenames.append(data[key][rnd_list[i]])
+                y_test.append(key)
+    X_train_filenames, y_train = _shuffle_samples(X_train_filenames,y_train)
+    X_test_filenames, y_test = _shuffle_samples(X_test_filenames, y_test)
     prediction_data = collections.namedtuple('Dataset',['X_train','y_train','X_test','y_test'])
-    genre_data = prediction_data()
+    genre_data = prediction_data(X_train=X_train_filenames,y_train=y_train,X_test=X_test_filenames,y_test=y_test)
+    return genre_data
 
 
-def _choose_filenames(train_amount = MAX_NUMBER, test_amount = int(MAX_NUMBER/10)):
-    _genre_to_filenames = joblib.load('new_genre_list')
-    _samples_tuple = _init_named_tuple()
-    _all_amount = train_amount + test_amount
-    for key in _genre_to_filenames.keys():
-        _file_amount = len(_genre_to_filenames[key])
-        if _file_amount >= _all_amount:
-            _range_gen = range(_file_amount)
-            _random_indices = random.sample(_range_gen, train_amount)
-            _filenames = _genre_to_filenames[key]
-            _samples_tuple.X_train, _samples_tuple.y_train = _fill_filenames_for_genre(train_amount,
-                                                                                       key, _filenames,
-                                                                                       _random_indices)
-            _samples_tuple.X_test, _samples_tuple.y_test = _fill_filenames_for_genre(test_amount, key, _filenames,
-                                                                                     _random_indices)
-    _shuffle_samples(_samples_tuple.X_train,_samples_tuple.y_train)
-    _shuffle_samples(_samples_tuple.X_test, _samples_tuple.y_test)
-    return _samples_tuple
-
-
-def _fill_filenames_for_genre(count, key, filenames, indices):
-    X = []
-    y = []
-    for i in range(count):
-        filename = filenames[indices[i]]
-        X.append(filename)
-        y.append(key)
-        del indices[i]
-    return X, y
-
-
-
-def norm_names(answers):
+def norm_names(y):
     i=0
-    _new_dic={}
+    new_dic={}
     y_new = []
     for elem in ALL_CLASSES:
-        _new_dic[elem] = i
+        new_dic[elem] = i
         i+=1
-    for elem in answers:
-        y_new.append(_new_dic[elem])
+    for elem in y:
+        y_new.append(new_dic[elem])
     return y_new
 
 
@@ -70,7 +54,7 @@ def _shuffle_samples(X,y):
     meta = list(zip(X,y))
     random.shuffle(meta)
     X[:], y[:] = zip(*meta)
-    return None
+    return X, y
 
 def _gen_docs(filenames):
     c = 0
@@ -87,8 +71,30 @@ def _get_bag_of_word(filenames):
     except FileNotFoundError:
         tf_idf = TfidfVectorizer()
     X = tf_idf.transform(_gen_docs(filenames))
-    return X.todense()
+    return X
 
+
+def _make_array(X):
+    X_cool = []
+    for elem in X:
+         X_cool.append(elem.toarray())
+    X_cool = numpy.asarray(X_cool)
+    X_cool = X_cool.reshape((X_cool.shape[0],-1))
+    return X_cool
+
+
+def _gen_bag_of_word(X,y):
+    print(y.shape)
+    while True:
+        rng_state = numpy.random.get_state()
+        index = numpy.arange(numpy.shape(X)[0])
+        print(index)
+        numpy.random.shuffle(index)
+        numpy.random.set_state(rng_state)
+        numpy.random.shuffle(y)
+        for idx,elem in zip(y,X[index, :]):
+            idx = numpy.array(idx)[numpy.newaxis]
+            yield (elem.toarray(), idx)
 
 def main():
     all_data = _choose_filenames()
@@ -101,18 +107,18 @@ def main():
 def keras(all_data):
     model = Sequential()
     X_train = _get_bag_of_word(all_data.X_train)
-    X_test = _get_bag_of_word(all_data.X_test)
-    y_train = norm_names(all_data.y_train)
-    y_test = norm_names(all_data.y_test)
-    model.add(Dense(128,input_dim=X_train.shape[1],activation='softmax'))
-    model.add(Dense(1))
-    # chunk=numpy.asarray(list(chunks(X_train)))
-    # print(chunk)
-    model.compile(optimizer='sgd',loss='mse')
-    model.fit(X_train, y_train, verbose=2,batch_size=1)
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred))
-
+    X_test = _make_array(_get_bag_of_word(all_data.X_test))
+    y_train = np_utils.to_categorical(norm_names(all_data.y_train))
+    y_test = np_utils.to_categorical(norm_names(all_data.y_test))
+    model.add(Dense(32,input_dim=X_test.shape[1]))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(14,activation='sigmoid'))
+    model.compile(optimizer='adam',loss='categorical_crossentropy')
+    model.fit_generator(_gen_bag_of_word(X_train,y_train),samples_per_epoch=252,verbose=2,nb_epoch=20)
+    y_pred = model.evaluate(X_test,y_test,batch_size=32)
+    y_what = model.predict_classes(X_test)
+    print(y_pred,y_test,y_train,y_what)
+    print(classification_report(norm_names(all_data.y_test),y_what))
 
 def chunks(array):
     for i in range(0,array.shape[1], 1000):
@@ -132,7 +138,7 @@ def log_reg(all_data):
     model = LogisticRegressionCV(verbose=1000, n_jobs=1, max_iter=10000)
     model.fit(_get_bag_of_word(all_data.X_train), all_data.y_train)
     y_pred = model.predict(_get_bag_of_word(all_data.X_test))
-    print(classification_report(all_data.y_test, y_pred))
+    print(classification_report(all_data.y_test, y_pred,target_names=ALL_CLASSES))
 
 
 def naive_bayes(all_data):
